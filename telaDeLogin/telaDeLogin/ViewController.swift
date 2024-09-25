7e8a74649dce3805d0e6094d793c15629b45a6c2
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-09-25 13:56:28 -0300

    Faz ajustes finos no layout na tela de login

--- telaDeLogin/telaDeLogin/ViewController.swift
+++ telaDeLogin/telaDeLogin/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var cadastrarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.keyboardType = .default
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.keyboardType = .default
+        passwordTextField.isSecureTextEntry = true
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
+    
+    
+    @IBAction func tappedCadastrarButton(_ sender: UIButton) {
+        print("botão está funcionando")
+    }
+    
 
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.blue.cgColor
+        textField.layer.borderWidth = 1
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 0
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
