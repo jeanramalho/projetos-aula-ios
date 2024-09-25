1fc74ddda1590639fbf6bbc7ef17c26591bdf821
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-09-25 11:17:29 -0300

    Adiciona primeiro projeto para estudo de IOS com a Backfront

---
+++ Tela Login StoryBoard/Tela Login StoryBoardTests/Tela_Login_StoryBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tela_Login_StoryBoardTests.swift
+//  Tela Login StoryBoardTests
+//
+//  Created by Jean Ramalho on 25/09/24.
+//
+
+import Testing
+@testable import Tela_Login_StoryBoard
+
+struct Tela_Login_StoryBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
