2390152029cf50a0d2513c330b8b31149e457a05
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-09-25 11:57:11 -0300

    Aula sobre IBOutlet

--- Tela Login StoryBoard/Tela Login StoryBoard/ViewController.swift
+++ Tela Login StoryBoard/Tela Login StoryBoard/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textLabelLogin: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabelLogin.text = "novo texto"
     }
 
 
