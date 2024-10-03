1a52eadc92d90943ecb1a4e187ea1c0d3bfa99c1
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-10-03 15:06:44 -0300

    Transita dados entre telas utilizando variaveis e a classe init do metodo proprio

--- NavigationAvancado/NavigationAvancado/ViewController.swift
+++ NavigationAvancado/NavigationAvancado/ViewController.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func goToScreen02(_ sender: UIButton) {
-        let VC: tela02VC? = UIStoryboard(name: "tela02VC", bundle: nil).instantiateViewController(withIdentifier: "tela02VC") as? tela02VC
+
+        
+        
+        // MARK: Método simples de navegação e passagem de dados entre telas
+        
+//        let VC: tela02VC? = UIStoryboard(name: "tela02VC", bundle: nil).instantiateViewController(withIdentifier: "tela02VC") as? tela02VC
         
         //abordagem pra chamar modal
         
@@ -24,9 +32,23 @@ class ViewController: UIViewController {
         
         //Abordagem pra chamar uma navigation Controller
         
+//        self.navigationController?.pushViewController(VC ?? UIViewController(), animated: true)
+//        
+//        VC?.name = self.nomeTextField.text
+        
+        
+        // MARK: Transintando dados entre telas de maneira correta
+        
+        let VC: tela02VC? = UIStoryboard(name: "tela02VC", bundle: nil).instantiateViewController(identifier: "tela02VC") { coder in return tela02VC(coder: coder, name: self.nomeTextField.text ?? "")
+            
+        }
+          
         self.navigationController?.pushViewController(VC ?? UIViewController(), animated: true)
+ 
+        
     }
     }
     
     
+
 
--- NavigationAvancado/NavigationAvancado/tela02VC.swift
+++ NavigationAvancado/NavigationAvancado/tela02VC.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class tela02VC: UIViewController {
 
+    
+    @IBOutlet weak var nomeLabel: UILabel!
+    
+    // MARK: Método simples de navegação e passagem de dados entre telas
+//    var name: String?
+    
+    let name: String
+    
+    init?(coder:NSCoder, name: String) {
+        self.name = name
+        super.init(coder: coder)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implanted.")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .green
+        
+        nomeLabel.text = name
     }
     
 
