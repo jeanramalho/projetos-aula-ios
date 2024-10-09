4d5fbbe4d0fe2f42fc0b2d87b11d5f3428731bfd
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-10-09 15:49:33 -0300

    Cria projeto de estudo TableView

---
+++ UITableView/UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Jean Ramalho on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
