fc46c48d91d913f425244fe86c5ae5b4e778d525
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-10-31 11:40:05 -0300

    Popula objeto criado

--- tableViewComObjeto/tableViewComObjeto/ViewController.swift
+++ tableViewComObjeto/tableViewComObjeto/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var listBrand: [Car] = [Car(brand: "Fiat", brandImage: UIImage(named: "fiat") ?? UIImage()),
+                            Car(brand: "Chevrolet", brandImage: UIImage(named: "chevrolet") ?? UIImage()),
+                            Car(brand: "Ford", brandImage: UIImage(named: "ford") ?? UIImage()),
+                            Car(brand: "BMW", brandImage: UIImage(named: "bmw") ?? UIImage()),
+                            Car(brand: "Ferrari", brandImage: UIImage(named: "ferrari") ?? UIImage()),]
+    
     @IBOutlet weak var tableView: UITableView!
     
 
@@ -16,7 +22,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
+    }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
 
