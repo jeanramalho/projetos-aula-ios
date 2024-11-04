537163504021f6a1c6de3050a8f4ba68214f5f06
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-04 11:49:45 -0300

    Cria primeiro app com elemento UICollectionView

---
+++ PrimeiroProjetoUICollectionViewCurso/PrimeiroProjetoUICollectionViewCurso/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoUICollectionViewCurso
+//
+//  Created by Jean Ramalho on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
