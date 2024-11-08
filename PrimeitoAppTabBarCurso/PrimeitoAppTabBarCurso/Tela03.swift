82c79a23dfbb458e0437cc49cced0c73255e8488
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-08 15:38:48 -0300

    Realiza ajustes finos no projeto de tabBar

--- PrimeitoAppTabBarCurso/PrimeitoAppTabBarCurso/Tela03.swift
+++ PrimeitoAppTabBarCurso/PrimeitoAppTabBarCurso/Tela03.swift
@@ -1,5 +1,5 @@
 //
-//  Tela03.swift
+//  ViewController.swift
 //  PrimeitoAppTabBarCurso
 //
 //  Created by Jean Ramalho on 08/11/24.
@@ -10,10 +10,11 @@ import UIKit
 class Tela03: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad() 
-        view.backgroundColor = .black
+        super.viewDidLoad()
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
-    
+
 
 }
+
