04052ca26c161cf48760dc4725c2dc80df9749d0
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-08 14:29:01 -0300

    Cria primeiro app utilizando tab bar

---
+++ PrimeitoAppTabBarCurso/PrimeitoAppTabBarCurso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeitoAppTabBarCurso
+//
+//  Created by Jean Ramalho on 08/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
